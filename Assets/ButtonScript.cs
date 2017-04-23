1774b5b0f75cc03d68a739408a35e8281fde86e4
Волков Иван Алексеевич <volkoivan74@gmail.com>
2017-04-23 13:11:16 +0500

    Added menu scene

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	public AudioClip hoverSound;
+	public AudioClip clickSound;
+	public Color hoverColor = new Color(0.8f,0.8f,0.8f);
+	public Color unhoverColor = new Color(1,1,1);
+
+	private Button button { get { return GetComponent<Button> (); } }
+
+	private Text text { get { return this.transform.GetChild (0).GetComponent<Text> (); } }
+	private AudioSource source { get { return GetComponent<AudioSource> (); } }
+
+	// Use this for initialization
+	void Start () {
+		gameObject.AddComponent<AudioSource>();
+		source.playOnAwake = false;
+	}
+
+	public void Unhover() {
+		text.color = unhoverColor;
+	}
+
+	public void PlayHover() {
+		text.color = hoverColor;
+		source.PlayOneShot (hoverSound);
+	}
+
+	public void PlayClick() {
+		source.PlayOneShot (clickSound);
+	}
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame() {
+		Invoke ("LoadLevel", 0.5f);
+	}
+
+	private void LoadLevel() {
+		Application.LoadLevel ("main");
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+}
